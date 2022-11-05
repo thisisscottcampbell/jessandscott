d14f78f20366695033baca409a4dc32b5a559ed4
Scott Campbell <thisisscottcampbell@gmail.com>
2022-11-05 16:47:01 -0400

    ok

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,10 +8,10 @@ const Header = ({ setIsMobile, isMobile }) => {
     // when the scroll is higher than 200 viewport height, add the scroll-header class to a tag with the header tag
     if (this.scrollY >= 80) {
       header.classList.add('scroll-header')
-      if (isMobile) setIsMobile(false)
+      //if (isMobile) setIsMobile(false)
     } else {
       header.classList.remove('scroll-header')
-      if (!isMobile) setIsMobile(true)
+      //if (!isMobile) setIsMobile(true)
     }
   })
 
