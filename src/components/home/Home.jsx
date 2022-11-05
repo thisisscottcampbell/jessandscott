a725557fe561be365495d9332bbaaff956413311
Scott Campbell <thisisscottcampbell@gmail.com>
2022-11-05 17:49:42 -0400

    ok

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -20,7 +20,7 @@ const getDeviceType = () => {
 const Home = ({ setIsMobile }) => {
   const deviceType = getDeviceType()
   const isMobile = deviceType == 'mobile' ? true : false
-  setIsMobile(isMobile)
+  setIsMobile(true)
   return (
     <section className="home section" id="home">
       <div className="home__container container grid">
